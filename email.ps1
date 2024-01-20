f1a577e4baea582b4b89cfc8e34d8183f6f7177e
Nick D <Ndefected15@gmail.com>
2024-01-19 20:02:24 -0500

    Updated code to be more modular and less redundant

--- email.ps1
+++ email.ps1
@@ -5,14 +5,11 @@
     [String]$body
 )
 
-# Get current timestamp
 $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
 
-# Format subject: capitalize the first letter of each word
 $subjectWords = $subject -split ' ' | ForEach-Object { $_.Substring(0,1).ToUpper() + $_.Substring(1) }
 $subject = $subjectWords -join ' '
 
-# Format body: capitalize the first letter of the first word and add a period at the end if there isn't one
 $bodyWords = $body -split ' ' | ForEach-Object { $_.Substring(0,1).ToUpper() + $_.Substring(1) }
 $body = $bodyWords -join ' '
 if (-not $body.EndsWith(".")) {
@@ -25,7 +22,6 @@ $message.From = 'Ndefected15@gmail.com'
 $message.To.Add($to)
 $message.Subject = $subject
 
-# Append timestamp to the body
 $message.Body = "$body`r`nTimestamp: $timestamp"
 
 $attach = New-Object Net.Mail.Attachment($file)
@@ -36,10 +32,8 @@ $smtp.EnableSsl = $true
 $smtp.Credentials = New-Object System.Net.NetworkCredential('ndefected15@gmail.com', 'efnl vgli zzxq quzv')
 $smtp.Send($message)
 
-# Output a message indicating the email was sent along with the timestamp
 Write-Output ""
 Write-Output ""
 Write-Output "The email was sent at: $timestamp"
 
-# Return to the batch file and call :anythingElse
 Exit 0
