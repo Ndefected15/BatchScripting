1822241223bc864cbbc56df0df6f8e7356c1cb76
Nick D <Ndefected15@gmail.com>
2024-01-19 19:58:52 -0500

    Updated Code to be more modular and less redundant, added functionality to Powershell file

--- batchScripting.bat
+++ batchScripting.bat
@@ -1,314 +1,279 @@
 @echo off
 setlocal enabledelayedexpansion
-goto :main
 
 :main
 setlocal
-    goto :readFiles
+call :readFiles
 endlocal
-goto :eof
 
 :readFiles
-setlocal
-    set /a count=0
+set /a count=0
+for /f %%i in ('dir /b /a-d *.csv') do (
+    set /a count+=1
+    set choice[!count!]=%%i
+)
 
-    for /f %%i in ('dir /b /a-d *.csv') do (
-        set /a count=count+1
-        set choice[!count!]=%%i
-    )
+echo.
 
-    echo.
-
-    if !switch!==1 (
-        goto :selectemail
-    ) else (
-        goto :task
-    )
-endlocal
-goto :eof
+if !switch!==1 (
+    call :selectemail
+) else (
+    goto :task
+)
 
 :task
-setlocal
-    set /a switch=0
+echo.
+echo.
+echo Would you like to concatenate two CSV files, add an ID column, or send a file through email:
+echo 1] Concatenate
+echo 2] Add ID Column
+echo 3] Send An Email
+echo 4] Quit
+echo.
 
-    echo Would you like to:
-    echo 1] Concatenate two CSV files
-    echo 2] Add an ID column to a CSV file
-    echo 3] Send a file through email
+set /p taskSelect=?
+
+if !taskSelect! geq 1 if !taskSelect! leq 4 (
+    call :case_%taskSelect%
+) else (
+    set type=task
     echo.
+    echo.
+    call :notValid
+)
 
-    set /p select=?
+:case_1
+set taskType=file1
+echo.
+echo.
+echo You have chosen Concatenate, is this correct? (y/n)
+echo.
+set /p choice=?
+goto :confirmTaskSelect
 
-    if !select!==1 goto :file1
-    if !select!==2 goto :selectFile
-    if !select!==3 (
-        set /a switch=1
-        goto :readFiles
-    )
+:case_2
+set taskType=ID
+echo.
+echo.
+echo You have chosen Add ID Column, is this correct? (y/n)
+echo.
+set /p choice=?
+goto :confirmTaskSelect
 
-    if NOT !select!==1 (
-        if NOT !select!==2 (
-            if NOT !select!==3 (
-                echo.
-                echo.
-                echo Not a valid response.
-                goto :task
-            )
-        )
-    )
-endlocal
-goto :eof
+:case_3
+set taskType=email
+echo.
+echo.
+echo You have chosen Send An Email, is this correct? (y/n)
+echo.
+set /p choice=?
+goto :confirmTaskSelect
+
+:case_4
+set taskType=quit
+echo.
+echo.
+echo You have chosen to quit, is this correct? (y/n)
+echo.
+set /p choice=?
+goto :confirmTaskSelect
+
+:confirmTaskSelect
+if /i !choice!==y (
+    echo.
+    echo Choice Confirmed.
+    goto :taskTypeSwitch
+) else if /i !choice!==n (
+    set taskType=task
+    goto :taskTypeSwitch
+) else (
+    goto :notValid
+)
+
+:taskTypeSwitch
+if !taskType!==taskSelect (
+    call :case_%taskSelect%
+)
+if !taskType!==file1 (
+    call :file1
+)
+if !taskType!==file2 (
+    call :file2
+)
+if !taskType!==ID (
+    call :IDFileSelect
+)
+if !taskType!==email (
+    set /a switch=1
+    call :readFiles
+)
+if !taskType!==task (
+    call :task
+)
+if !taskType!==quit (
+    goto :end
+)
 
 :file1
-setlocal
-    Echo.
-
-    echo Select File 1:
-
-    for /l %%x in (1,1,!count!) do (
-        echo  %%x] !choice[%%x]!
-    )
-
-    echo.
-
-    set /p select1=?
-
-    echo.
-
-    if !select1! gtr 0 (
-        goto :1true
-    ) else (
-        goto :1false
-    )
-
-    :1true
-        if !select1! leq !count! (
-            goto :1true2
-        ) else (
-            goto :1false
-        )
-
-    :1true2
-        echo You chose !choice[%select1%]!
-        echo.
-        goto :file2
-
-    :1false
-        echo not a valid response
-        goto :file1
-endlocal
-goto :eof
+set taskType=file1
+echo.
+echo Select File 1:
+call :fileSelect
 
 :file2
-setlocal
-    echo Select File 2:
+set taskType=file2
+echo.
+echo Select File 2:
+call :fileSelect
 
-    for /l %%x in (1,1,!count!) do (
-        echo  %%x] !choice[%%x]!
-    )
+:IDFileSelect
+set taskType=ID
+echo.
+echo Select a File:
+call :fileSelect
 
-    echo.
+:fileSelect
+for /l %%x in (1,1,!count!) do (
+    echo  %%x] !choice[%%x]!
+)
+echo.
+set /p fileSelect=?
+echo.
+if !fileSelect! gtr 0 (
+    call :confirmFileSelect
+) else (
+    call :notValid
+)
 
-    set /p select2=?
+:confirmFileSelect
+echo.
+echo You have chosen !choice[%fileSelect%]!, is this correct? (y/n)
+echo.
+set /p choice=?
+if /i !choice!==y (
+    call :confirmFile_%choice%
+)
+if /i !choice!==n (
+    call :confirmFile_%choice%
+) else (
+    set type=file
+    set fileType=confirmFileSelect
+    goto :notValid
+)
 
-    echo.
+:confirmFile_y
+call :grtCheck
 
-    if !select2! gtr 0 (
-        goto :2true
-    ) else (
-        goto :2false
-    )
+:confirmFile_n
+call :taskTypeSwitch
 
-    :2true
-        if !select2! leq !count! (
-            goto :2true2
-        ) else (
-            goto :2false
-        )
+:grtCheck
+if !fileSelect! leq !count! (
+    goto :leqCheck
+) else (
+    goto :notValid
+)
 
-    :2true2
-        echo You chose !choice[%select2%%]!
-        echo.
-        goto :concatenate
+:leqCheck
+echo.
+echo.
+echo You chose !choice[%fileSelect%]!
+if !taskType!==file1 (
+    set select1=!fileSelect!
+    goto :file2
+)
+if !taskType!==file2 (
+    set select2=!fileSelect!
+    goto :concatenate
+)
+if !taskType!==ID (
+    goto :transform
+)
+if !taskType!==email (
+    call :emailInfo
+    call :sendemail
+)
 
-    :2false
-        echo not a valid response
-        goto :file2
-endlocal
-goto :eof
-
-:selectFile
-setlocal
-    Echo.
-
-    echo Select a File:
-
-    for /l %%x in (1,1,!count!) do (
-        echo  %%x] !choice[%%x]!
-    )
-
-    echo.
-
-    set /p select=?
-
-    echo.
-
-    if !select! gtr 0 (
-        goto :selecttrue
-    ) else (
-        goto :selectfalse
-    )
-
-    :selecttrue
-        if !select! leq !count! (
-            goto :selecttrue2
-        ) else (
-            goto :selectfalse
-        )
-
-    :selecttrue2
-        echo You chose !choice[%select%]!
-        echo.
-        set /a count=0
-        goto :transform
-
-    :selectfalse
-        echo not a valid response
-        goto :selectFile
-endlocal
-goto :eof
+:fileTypeSwitch
+if !fileType!==fileSelect (
+    call :fileSelect
+)
+if !fileType!==confirmFileSelect (
+    call :confirmFileSelect
+)
 
 :concatenate
-setlocal
-    copy !choice[%select1%]! + !choice[%select2%]! Concat.csv
-
-    echo.
-    echo.
-    echo Complete!
-
-    goto :email
-endlocal
-goto :eof
+copy !choice[%select1%]! + !choice[%select2%]! Concat.csv
+echo.
+echo Complete!
+goto :anythingElse
 
 :transform
-setlocal
-    echo.
+echo.
+echo.
+echo Adding ID column to !choice[%fileSelect%]!
+set i=0
+echo ID > output.csv
+(for /f "usebackq delims=" %%a in (!choice[%fileSelect%]!) do (
+    echo !i!,%%a >> output.csv
+    set /A "i=(i+1)"
+))
+echo.
+echo.
+echo ID column added to !choice[%fileSelect%]!
+goto :anythingElse
 
-    echo Adding ID column to !choice[%select%]!
+:anythingElse
+echo.
+echo.
+echo Would you like to do anything else? (y/n)
+echo.
+set /p choice=?
+goto :choice_%choice%
 
-    set i=0
+:choice_y
+goto :task
 
-    echo ID > output.csv
-
-    (for /f "UseBackQ Delims=" %%a in (!choice[%select%]!) do (
-        echo !i!,%%a >> output.csv
-        set /A "i=(i+1)"
-    ))
-	
-    echo.
-    echo Complete!
-
-    goto :email
-endlocal
-goto :eof
-
-:email
-setlocal
-    echo.
-    echo.
-
-    echo Would you like to email a file? (y/n)
-
-    echo.
-
-    set /p response=
-
-    if !response!==y (
-        set /a switch=1
-        goto :readFiles
-    )
-
-    if !response!==n goto :end
-endlocal
-goto :eof
+:choice_n
+goto :end
 
 :selectemail
-setlocal
-    set /a switch=0
-
-    Echo.
-
-    echo Select a File to email:
-
-    for /l %%x in (1,1,!count!) do (
-        echo  %%x] !choice[%%x]!
-    )
-
-    echo.
-
-    set /p select=?
-
-    echo.
-
-    if !select! gtr 0 (
-        goto :3true
-    ) else (
-        goto :3false
-    )
-
-    :3true
-        if !select! leq !count! (
-            goto :3true2
-        ) else (
-            goto :3false
-        )
-
-    :3true2
-        echo You chose !choice[%select%]!
-        echo.
-
-        set /a count=0
-        goto :emailInfo
-
-        goto :sendemail
-
-    :3false
-        echo not a valid response
-        goto :selectemail
-endlocal
-goto :eof
+set type=email
+Echo.
+echo Select a File to email:
+call :fileSelect
 
 :emailInfo
-setlocal
-    echo.
-    echo Enter email you're sending file to:
-    echo.
-
-    set /p to=?
-
-    echo.
-
-    echo Enter the Subject:
-    echo.
-
-    set /p subject=?
-
-    echo.
-
-    echo Enter the body:
-    echo.
-
-    set /p body=?
-
-    goto :sendemail
-endlocal
-goto :eof
+echo.
+echo Enter email you're sending file to:
+echo.
+set /p to=?
+echo.
+echo Enter the Subject:
+echo.
+set /p subject=?
+echo.
+echo Enter the body:
+echo.
+set /p body=?
 
 :sendemail
-setlocal
-    PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './email.ps1'" !choice[%select%]! !to! !subject! "!body!"
-endlocal
-goto :eof
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './email.ps1' !choice[%fileSelect%]! !to! '!subject!' '!body!'"
+goto :anythingElse
+
+:notValid
+echo.
+echo Not a valid response.
+echo.
+if !type!==task (
+    call :taskTypeSwitch
+)
+if !type!==file (
+    call :fileTypeSwitch
+)   
 
 :end
-setlocal
+echo.
+echo Thank you.
+goto :eof
+exit /b
--- email.ps1
+++ email.ps1
@@ -1,41 +1,45 @@
 ﻿Param(
-
     [String]$file,
-
     [String]$to,
-
     [String]$subject,
-
     [String]$body
+)
 
-    )
+# Get current timestamp
+$timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
 
+# Format subject: capitalize the first letter of each word
+$subjectWords = $subject -split ' ' | ForEach-Object { $_.Substring(0,1).ToUpper() + $_.Substring(1) }
+$subject = $subjectWords -join ' '
 
-$smtpserver = "smtp.gmail.com" 
+# Format body: capitalize the first letter of the first word and add a period at the end if there isn't one
+$bodyWords = $body -split ' ' | ForEach-Object { $_.Substring(0,1).ToUpper() + $_.Substring(1) }
+$body = $bodyWords -join ' '
+if (-not $body.EndsWith(".")) {
+    $body += "."
+}
 
-$message = new-object System.Net.Mail.MailMessage
-
+$smtpserver = "smtp.gmail.com"
+$message = New-Object System.Net.Mail.MailMessage
 $message.From = 'Ndefected15@gmail.com'
-
 $message.To.Add($to)
-
 $message.Subject = $subject
 
-$attach = new-object Net.Mail.Attachment($file)
+# Append timestamp to the body
+$message.Body = "$body`r`nTimestamp: $timestamp"
 
+$attach = New-Object Net.Mail.Attachment($file)
 $message.Attachments.Add($attach)
 
-$message.body = "$body"
-
-$smtp = new-object Net.Mail.SmtpClient($smtpserver, 587)
-
+$smtp = New-Object Net.Mail.SmtpClient($smtpserver, 587)
 $smtp.EnableSsl = $true
-
-$smtp.Credentials = New-Object System.Net.NetworkCredential('ndefected15@gmail.com', 'app-pw')
-
+$smtp.Credentials = New-Object System.Net.NetworkCredential('ndefected15@gmail.com', 'efnl vgli zzxq quzv')
 $smtp.Send($message)
 
+# Output a message indicating the email was sent along with the timestamp
+Write-Output ""
+Write-Output ""
+Write-Output "The email was sent at: $timestamp"
 
-""
-""
-"The email was sent!"
+# Return to the batch file and call :anythingElse
+Exit 0
